c835d753fb558232a93263a8ce4dab29265fddae
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-01-22 14:03:25 +0900

    CRUD機能完了

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -2,6 +2,10 @@
 const usersModule = (() => {
   const BASE_URL = "http://localhost:3000/api/v1/users"
 
+  // ヘッダーの設定
+  const headers = new Headers()
+  headers.set("Content-Type", "application/json")
+
   return {
     fetchAllUsers: async () => {
       const res = await fetch(BASE_URL)
@@ -16,8 +20,81 @@ const usersModule = (() => {
                         <td>${user.date_of_birth}</td>
                         <td>${user.created_at}</td>
                         <td>${user.updated_at}</td>
+                        <td><a href="edit.html?uid=${user.id}">編集</a></td>
                       </tr>`
         document.getElementById('users-list').insertAdjacentHTML('beforeend', body)
+      }
+    },
+    createUser: async () => {
+      const name = document.getElementById('name').value
+      const profile = document.getElementById('profile').value
+      const dateOfBirth = document.getElementById('date_of_birth').value
+
+      // リクエストのbody
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth: dateOfBirth
+      }
+
+      const res = await fetch(BASE_URL, {
+        method: "POST",
+        headers: headers,
+        // JavaScirptのオブジェクトをJSON文字列に変換
+        body: JSON.stringify(body)
+      })
+
+      const resJson = await res.json()
+
+      alert(resJson.message)
+      window.location.href = "/"
+    },
+    setExistingValue: async (uid) => {
+      const res = await fetch(BASE_URL + "/" + uid)
+      const resJson = await res.json()
+
+      document.getElementById('name').value = resJson.name
+      document.getElementById('profile').value = resJson.profile
+      document.getElementById('date_of_birth').value = resJson.date_of_birth
+    },
+
+    saveUser: async (uid) => {
+      const name = document.getElementById('name').value
+      const profile = document.getElementById('profile').value
+      const dateOfBirth = document.getElementById('date_of_birth').value
+
+      // リクエストのbody
+      const body = {
+        name: name,
+        profile: profile,
+        date_of_birth: dateOfBirth
+      }
+
+      const res = await fetch(BASE_URL + "/" + uid, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify(body)
+      })
+
+      const resJson = await res.json()
+
+      alert(resJson.message)
+      window.location.href = "/"
+    },
+    deleteUser: async (uid) => {
+      const ret = window.confirm('このユーザを削除しますか？')
+
+      if (!ret) {
+        return false
+      } else {
+        const res = await fetch(BASE_URL + "/" + uid, {
+          method: "DELETE",
+          headers: headers,
+        })
+
+        const resJson = await res.json()
+        alert(resJson.message)
+        window.location.href = '/'
       }
     }
   }
